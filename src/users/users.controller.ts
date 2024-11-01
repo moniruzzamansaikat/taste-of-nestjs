296b458d6867b47e3b4fcde0168a1a038528e01e
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-11-01 20:45:20 +0600

    guard the whole controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,16 +4,15 @@ import { User } from './user.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<User> {
     return this.usersService.findOneById(id);
